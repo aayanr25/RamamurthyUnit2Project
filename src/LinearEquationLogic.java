c2a03bae2b036da9ec9db12af01f40580fda5bc7
BT_1N3_10 <sample@bths.edu>
2023-10-17 09:27:58 -0400

    started logic class

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
 public class LinearEquationLogic {
+    private LinearEquation line;
+    private Scanner scan;
+    public LinearEquationLogic() {
+        line = null;
+        scan = new Scanner(System.in);
+    }
+    public void start() {
+
+    }
+    private void getCoordinates() {
+        System.out.print("Enter first coordinate: ");
+        String coordinate = scan.nextLine();
+
+    }
 }
